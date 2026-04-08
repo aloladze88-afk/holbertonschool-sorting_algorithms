6ae346ab52a88e3cef06d1e3b58af88fd21b45fc
Kaiserr999 <I.camelin74@gmail.com>
2026-04-08 20:16:02 +0200

    add

---
+++ 3-quick_sort.c
@@ -0,0 +1,74 @@
+#include "sort.h"
+
+/**
+ * partition - Partitions the array using Lomuto partition scheme
+ * @array: array to partition
+ * @low: starting index
+ * @high: ending index
+ * @size: size of the array for printing
+ *
+ * Return: the index of the pivot element in its final sorted position
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int temp;
+	int j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+	if (i + 1 != high)
+	{
+		temp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+
+/**
+ * quick_sort_helper - Recursive helper function for quick sort
+ * @array: array to sort
+ * @low: starting index
+ * @high: ending index
+ * @size: size of the array for printing
+ */
+void quick_sort_helper(int *array, int low, int high, size_t size)
+{
+	int pivot_index;
+
+	if (low < high)
+	{
+		pivot_index = partition(array, low, high, size);
+		quick_sort_helper(array, low, pivot_index - 1, size);
+		quick_sort_helper(array, pivot_index + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - sorts an array of integers in ascending order
+ *              using the Quick sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quick_sort_helper(array, 0, size - 1, size);
+}
